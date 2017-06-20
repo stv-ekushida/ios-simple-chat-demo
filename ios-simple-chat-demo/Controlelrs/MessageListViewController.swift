8dd6494703e2f4e90afbabdb411310fe94e29d30
e.kushida <e.kushida@st-ventures.jp>
2017-06-20 10:30:47 +0900

    Add MessageListViewController

---
+++ ios-simple-chat-demo/Controlelrs/MessageListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MessageListViewController.swift
+//  ios-simple-chat-demo
+//
+//  Created by Eiji Kushida on 2017/06/20.
+//  Copyright © 2017年 Eiji Kushida. All rights reserved.
+//
+
+import UIKit
+
+final class MessageListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
