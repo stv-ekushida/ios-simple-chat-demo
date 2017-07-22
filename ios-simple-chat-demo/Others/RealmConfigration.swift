28a3ba1e377f8339e2d4c4344a991e7254e1b1a0
e.kushida <e.kushida@st-ventures.jp>
2017-07-22 11:59:57 +0900

    Update 実行時のワーニングを消す

---
+++ ios-simple-chat-demo/Others/RealmConfigration.swift
@@ -0,0 +1,9 @@
+//
+//  RealmConfigration.swift
+//  ios-simple-chat-demo
+//
+//  Created by Eiji Kushida on 2017/07/22.
+//  Copyright © 2017年 Eiji Kushida. All rights reserved.
+//
+
+import Foundation
